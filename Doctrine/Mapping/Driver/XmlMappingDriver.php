d5a2615b83ebfe847aa2afe63c3c227d7c713246
Florian Semm <florian.semm@gmx.de>
2012-12-02 11:30:22 +0100

    implement xml mapping driver and tests

--- Doctrine/Mapping/Driver/XmlMappingDriver.php
+++ Doctrine/Mapping/Driver/XmlMappingDriver.php
@@ -11,7 +11,44 @@ class XmlMappingDriver extends AbstractMappingDriver {
 		foreach ($files as $file) {
 			$path = $this->dir.'/'.$file->getFilename();
 			$xml = simplexml_load_file($path);
+			
+			$children = $xml->children();
+			$documentNode = $children[0];
+			
+			$attributes = $documentNode->attributes();
+
+			$mapping = array();
+			$mapping['attributes']['repository'] = $this->elementToString($attributes['repository']);
+			$mapping['attributes']['boost'] = $this->elementToString($attributes['boost']);
+			$mapping['attributes']['entity'] = $this->elementToString($attributes['entity']);
+			
+			$mapping = $this->loadFields($documentNode, $mapping);
+			
+			$this->mappings[] = $mapping;
 		}
+	}
+	
+	private function loadFields(\SimpleXMLElement $documentNode, array $mapping) {
+		foreach ($documentNode->field as $fieldNode) {
+			$fieldAttributes = $fieldNode->attributes();
+		
+			$field = array();
+			$field['name'] = $this->elementToString($fieldAttributes['name']);
+			$field['type'] = $this->elementToString($fieldAttributes['type']);
+			$field['property'] = $this->elementToString($fieldAttributes['property']);
+
+			$mapping['fields'][] = $field;
+		}	
+
+		return $mapping;
+	}
+	
+	private function elementToString(\SimpleXMLElement $element = null) {
+		if (is_null($element)) {
+			return '';
+		}
+		
+		return $element->__toString();
 	}
 }
 
--- Tests/Doctrine/Driver/XmlMappingDriverTest.php
+++ Tests/Doctrine/Driver/XmlMappingDriverTest.php
@@ -20,6 +20,22 @@ class XmlMappingDriverTest extends \PHPUnit_Framework_TestCase {
 		
 		
 		$this->assertEquals(1, count($xmlDriver->getMappings()));
+		$mappings = $xmlDriver->getMappings();
+		
+		$mapping = array_pop($mappings);
+		$this->assertArrayHasKey('attributes', $mapping);
+		
+		$this->assertArrayHasKey('boost', $mapping['attributes']);
+		$this->assertArrayHasKey('entity', $mapping['attributes']);
+		$this->assertArrayHasKey('repository', $mapping['attributes']);
+		
+		$this->assertArrayHasKey('fields', $mapping);
+		$this->assertEquals(2, count($mapping['fields']));
+		$field = array_pop($mapping['fields']);
+		
+		$this->assertArrayHasKey('name', $field);
+		$this->assertArrayHasKey('type', $field);
+		$this->assertArrayHasKey('property', $field);
 	}
 }
 
